d2ebc92d6bb3d327c17ab65421c8353a5c7564f4
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-31 09:14:39 +0600

    updated

--- src/Pages/Reviews/SingleReview.js
+++ src/Pages/Reviews/SingleReview.js
@@ -8,11 +8,10 @@ const SingleReview = (props) => {
         <div style={{fontFamily:"aleo", letterSpacing:"1px"}}>
             <div className='card card-body bg-base-200 h-3/3 text-black text-center my-3'>
           <h2 style={{fontFamily:"Aleo"}} className='text-xl font-bold mt-3'> {name}  </h2>
+          <hr className='h-1 bg-blue-300' />
           <p className='text-left'>{review}</p>
         </div>
   </div>
-// </div>
-        // </div>
     );
 };
 
